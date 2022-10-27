23fb179ce622039b5490ca33c0f0f9eba6d455f5
jafar260698 <jafar_temirov@mail.ru>
2022-10-27 12:46:55 +0500

    - leetcode29.dart fixed

--- bin/leetcode/leetcode29.dart
+++ bin/leetcode/leetcode29.dart
@@ -1,38 +1,30 @@
 
 
 void main(List<String> arguments) {
-  print(divide(1, 1));
+  print(divide(-2147483648, -1));
 }
 
 int divide(int dividend, int divisor) {
 
-  if (dividend == 0) return 0;
-
   bool isNegative = false;
+  int counter = 0;
+
+
   int a = dividend.abs();
   int b = divisor.abs();
-  int lastNum = 0;
+
+  if(b > a) return 0;
   if((dividend < 0 && divisor > 0) || (dividend >0 && divisor < 0)){ isNegative = true; }
 
-
   if(a == 1 || b == 1) {
-    if ((2147483647 <= a && a <= 2147483648) || (2147483647 <= b && b <= 2147483648)) {
-      return 2147483647 * (isNegative ? -1 : 1);
-    }
+    if(dividend == -2147483648 && divisor == -1) return 2147483647;
     return dividend * divisor;
   }
 
-    while (a >= b) {
-      a = a - b;
-    }
+  while (a >= b) {
+    a = a - b;
+    counter++;
+  }
 
-    lastNum = (dividend.abs() - a);
-    int i = 0;
-
-    while(lastNum > 0) {
-      i++;
-      lastNum = lastNum - b;
-    }
-
-    return i  * (isNegative ? -1 : 1);
+  return counter  * (isNegative ? -1 : 1);
 }
